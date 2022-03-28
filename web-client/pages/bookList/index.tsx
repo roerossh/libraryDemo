00846cce8a998a6ef90877ebc1442d9dca3d902a
dengxue03 <dengxue03@kuaishou.com>
2022-03-28 11:00:11 +0800

    chore: test

--- web-client/pages/bookList/index.tsx
+++ web-client/pages/bookList/index.tsx
@@ -1,31 +1,31 @@
-import * as React from "react";
+import * as React from 'react';
 import { Component } from 'react';
 import { observer } from 'mobx-react';
-import { List, Card, Layout } from "antd";
+import { List, Card, Layout } from 'antd';
 import BookStore from './store';
-import { IBook } from "@server/interfaces/books";
-import BookItem from "components/BookItem";
+import { IBook } from '@server/interfaces/books';
+import BookItem from 'components/BookItem';
 import './index.scss';
 
-const {Header, Content} = Layout;
+const { Header, Content } = Layout;
 const { Meta } = Card;
+console.log();
 @observer
 export default class BookList extends Component {
-
-    onSearch = (val: string) =>{
-        BookStore.queryBookList(val);
-    }
-    render() {
-        const { keyword, bookList } = BookStore;
-        return (
-            <Layout className="container">
-            <Content>
-                <List
-                dataSource={bookList}
-                renderItem={(item: IBook) => <BookItem key={item.id} item={item}/>}>
-                </List> 
-            </Content>         
-            </Layout>
-        )
-    }
+  onSearch = (val: string) => {
+    BookStore.queryBookList(val);
+  };
+  render() {
+    const { keyword, bookList } = BookStore;
+    return (
+      <Layout className="container">
+        <Content>
+          <List
+            dataSource={bookList}
+            renderItem={(item: IBook) => <BookItem key={item.id} item={item} />}
+          ></List>
+        </Content>
+      </Layout>
+    );
+  }
 }
